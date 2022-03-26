1b522e3922e54ec4f8554cbda2e8c62717dfc215
SuzuSys <sigmatics@outlook.jp>
2022-03-27 04:58:30 +0900

    second commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  api: {
+    wordproject3: {
+      GraphQLAPIIdOutput: "string";
+      GraphQLAPIEndpointOutput: "string";
+    };
+  };
+  auth: {
+    wordproject3420c3ca3420c3ca3: {
+      IdentityPoolId: "string";
+      IdentityPoolName: "string";
+      UserPoolId: "string";
+      UserPoolArn: "string";
+      UserPoolName: "string";
+      AppClientIDWeb: "string";
+      AppClientID: "string";
+    };
+  };
+};
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <authenticator>
+    <v-app></v-app>
+  </authenticator>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Authenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
+import { Amplify } from "aws-amplify";
+import awsconfig from "aws-exports";
+Amplify.configure(awsconfig);
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Authenticator,
+  },
+});
+</script>
