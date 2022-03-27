40fed49f2bc6a2eb5486f400b645e934074d8d6c
SuzuSys <sigmatics@outlook.jp>
2022-03-28 07:10:45 +0900

    [fix] App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,10 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import { Amplify } from "aws-amplify";
-import awsconfig from "./aws-exports";
+import awsconfig from "@/aws-exports";
 Amplify.configure(awsconfig);
 import { Authenticator } from "@aws-amplify/ui-vue";
-import AppChild from "./AppChild.vue";
+import AppChild from "@/AppChild.vue";
 import "@aws-amplify/ui-vue/styles.css";
 
 export default defineComponent({
