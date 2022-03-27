ab7d6db04a034772f5820014428e11d5164a5996
SuzuSys <sigmatics@outlook.jp>
2022-03-28 07:06:07 +0900

    [fix] App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,25 +6,18 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { Authenticator } from "@aws-amplify/ui-vue";
-import "@aws-amplify/ui-vue/styles.css";
 import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-vue";
 import awsconfig from "./aws-exports";
 Amplify.configure(awsconfig);
-import { useI18n } from "vue-i18n";
 import AppChild from "./AppChild.vue";
+import "@aws-amplify/ui-vue/styles.css";
 
 export default defineComponent({
   name: "App",
   components: {
     Authenticator,
     AppChild,
-  },
-  setup() {
-    const { t } = useI18n();
-    return {
-      t,
-    };
   },
 });
 </script>
