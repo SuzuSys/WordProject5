9579ea85678d5864a7d592de48001e6b0d3bfd02
SuzuSys <sigmatics@outlook.jp>
2022-03-28 16:17:40 +0900

    [add] EditAccountForm.vue EditProfileForm.vue

---
+++ src/components/EditAccountForm.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-card class="mx-auto my-12" max-width="500" variant="outlined">
+    <v-card-title>Edit Account</v-card-title>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "EditAccountForm",
+});
+</script>
--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,11 +1,18 @@
 <template>
-  <h1>Account</h1>
+  <edit-account-form></edit-account-form>
+  <edit-profile-form></edit-profile-form>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import EditAccountForm from "@/components/EditAccountForm.vue";
+import EditProfileForm from "@/components/EditProfileForm.vue";
 
 export default defineComponent({
   name: "AccountView",
+  components: {
+    EditAccountForm,
+    EditProfileForm,
+  },
 });
 </script>
