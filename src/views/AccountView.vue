9e20e32e325db80eb653b262f3bdbfda4a40e1a4
SuzuSys <sigmatics@outlook.jp>
2022-04-17 20:06:03 +0900

    [update] AccountView.vue

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,18 +1,9 @@
+<script setup lang="ts">
+import EditAccountForm from "@/components/EditAccountForm.vue";
+import EditProfileForm from "@/components/EditProfileForm.vue";
+</script>
+
 <template>
   <edit-account-form></edit-account-form>
   <edit-profile-form></edit-profile-form>
 </template>
-
-<script lang="ts">
-import { defineComponent } from "vue";
-import EditAccountForm from "@/components/EditAccountForm.vue";
-import EditProfileForm from "@/components/EditProfileForm.vue";
-
-export default defineComponent({
-  name: "AccountView",
-  components: {
-    EditAccountForm,
-    EditProfileForm,
-  },
-});
-</script>
