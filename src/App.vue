766b467511a36f2effefe3b69b7040a2f5dfd3a6
SuzuSys <sigmatics@outlook.jp>
2022-04-17 19:46:38 +0900

    [update] App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
+<script setup lang="ts">
+import { Authenticator } from "@aws-amplify/ui-vue";
+import AppChild from "@/AppChild.vue";
+import "@aws-amplify/ui-vue/styles.css";
+import { Amplify } from "aws-amplify";
+import awsconfig from "@/aws-exports";
+Amplify.configure(awsconfig);
+</script>
+
 <template>
   <authenticator>
     <app-child></app-child>
   </authenticator>
 </template>
-
-<script lang="ts">
-import { defineComponent } from "vue";
-import { Amplify } from "aws-amplify";
-import awsconfig from "@/aws-exports";
-Amplify.configure(awsconfig);
-import { Authenticator } from "@aws-amplify/ui-vue";
-import AppChild from "@/AppChild.vue";
-import "@aws-amplify/ui-vue/styles.css";
-
-export default defineComponent({
-  name: "App",
-  components: {
-    Authenticator,
-    AppChild,
-  },
-});
-</script>
